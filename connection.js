05bbb83fc8a19a51ae4ddc7245317054c8c28535
matihullahIT <matihurrehman.official@gmail.com>
2025-04-13 15:03:08 +0500

    Refactor MongoDB connection to use environment variable for URL

--- connection.js
+++ connection.js
@@ -1,8 +1,15 @@
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables
 
-    mongoose.connection('mongodb://localhost:27017/quizera', {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=>console.log("mongodb is connected"))
-    .catch(err){
-    console.log(err);
-}
-module.exports=mongoose.connection;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const DbConnection = () => {
+    mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log("MongoDB is connected"))
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+DbConnection();
+module.exports = DbConnection;
