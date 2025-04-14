09c4263b7cd41119f1abb94c70cbb32bf3ae9822
matihullahIT <matihurrehman.official@gmail.com>
2025-04-15 02:17:42 +0500

    refactor: standardize port assignment and improve logging message in index.js

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import dbconnection from './connection.js';
 import dotenv from 'dotenv';
-import router from './router/index.js'; // Import the router
+import dbconnection from './connection.js';
+import router from './router/index.js';
 
 dotenv.config();
-const app = express();
-const PORT = process.env.PORT;
 
-// Middleware to parse JSON request bodies
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(cors());
-
-// Use the router for handling routes
 app.use('/api', router);
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
     dbconnection();
 });
